04d4958abe9e022340904dcac08ac460f627054f
E14 <1640391+E14@users.noreply.github.com>
2021-08-19 11:56:45 +0200

    Unit test for killing a process

--- test/rascal_test.exs
+++ test/rascal_test.exs
@@ -1,8 +1,24 @@
 defmodule RascalTest do
 	use ExUnit.Case
 	doctest Rascal
+	alias Rascal.Pidify
 
-	test "greets the world" do
-		# assert Rascal.hello() == :world
+	test "can kill agent" do
+		{state_pid, state_pn} = RascalTest.State.start_link();
+		assert state_pid == Pidify.pid_from_name(state_pn)
+		Rascal.prank!(state_pid)
+		
+		ref = Process.monitor(state_pid)
+
+		assert_receive({:DOWN, ^ref, :process, ^state_pid, _reason}, 500)
+	end
+
+	defmodule State do
+		use Agent
+
+		def start_link() do
+			{:ok, pid} = Agent.start(fn -> 1 end, name: __MODULE__)
+			{pid, __MODULE__}
+		end
 	end
 end
